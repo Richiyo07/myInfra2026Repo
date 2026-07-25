73ab339209cec8d497c79f2f45ed34069a140819
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2026-07-25 02:46:03 -0400

    Refactor SonarQube security group configuration

--- sonar.tf
+++ sonar.tf
@@ -8,38 +8,36 @@ resource "aws_instance" "mySonarInstance" {
       }
     }
 
-resource "aws_security_group" "sonar_sg" {
-  name        = "sonar-sg"
-  description = "Security group for SonarQube server"
-  vpc_id      = aws_vpc.main.id
+ resource "aws_security_group" "sonar-sg-2022" {
+      name        = "security_sonar_group_2022"
+      description = "security group for Sonar"
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Replace with your public IP
-  }
+      ingress {
+        from_port   = 9000
+        to_port     = 9000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
 
-  ingress {
-    description = "SonarQube Web UI"
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Restrict if possible
-  }
+     ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+     # outbound from Sonar server
+      egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
 
-  tags = {
-    Name = "sonar-sg"
-  }
-}
+      tags= {
+        Name = "security_sonar"
+      }
+    }
 
 # Create Elastic IP address for Sonar instance
 resource "aws_eip" "mySonarInstance" {
