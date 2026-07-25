db9f644345cb4523120d1cdc06a3e5fc7540e9ee
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2026-07-25 02:12:04 -0400

    Change default AMI ID in variables.tf
    Updated the default AMI ID for the Ubuntu EC2 instance.

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0b3c69cd8456a2e2e" 
+    default     = "ami-0ea1cddefe0c4aed5" 
 }
 variable "versioning" {
     type        = bool
