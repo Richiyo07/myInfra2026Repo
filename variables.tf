442dd559408852a7c2a94b0e1271755a575a4501
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2026-07-25 02:00:25 -0400

    Update AWS region and instance type defaults

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "us-east-2" 
 }
 
 variable "key_name" { 
@@ -10,7 +10,7 @@ variable "key_name" {
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.micro" 
+    default     =  "t3.small" 
 }
 
 variable "security_group" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-0b3c69cd8456a2e2e" 
 }
 variable "versioning" {
     type        = bool
