b5b0626b72e8d6980793ea8f987a87d6482e5a20
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2026-07-25 01:51:07 -0400

    Change S3 backend region from us-east-1 to us-east-2

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-aws-s3-bucket-dodo"
     key = "main"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "my_demo_bucket_dodo"
   }
 }
