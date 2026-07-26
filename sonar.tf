346efc6c733f339d6e4303365a97afe79ca4c402
Richiyo07 <139646928+Richiyo07@users.noreply.github.com>
2026-07-26 12:15:06 -0400

    Create EC2 instance and security group for SonarQube
    Added an EC2 instance and associated security group for SonarQube.

--- sonar.tf
+++ sonar.tf
@@ -1,35 +1,45 @@
-resource "aws_security_group" "sonar_sg" {
-  name   = "sonar-security-group"
-  vpc_id = aws_vpc.main.id
+resource "aws_instance" "mySonarInstance" {
+      ami           = "ami-0ee23bfc74a881de5"
+      key_name = var.key_name
+      instance_type = "t3.small"
+      vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
+      tags= {
+        Name = "sonar_instance"
+      }
+    }
 
-  ingress {
-    description = "SSH access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ resource "aws_security_group" "sonar-sg-2022" {
+      name        = "security_sonar_group_2022"
+      description = "security group for Sonar"
 
-  ingress {
-    description = "SonarQube Web"
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+      ingress {
+        from_port   = 9000
+        to_port     = 9000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+     ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
 
-  tags = {
-    Name = "sonar-sg"
-  }
-}
+     # outbound from Sonar server
+      egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
 
+      tags= {
+        Name = "security_sonar"
+      }
+    }
+
+# Create Elastic IP address for Sonar instance
 resource "aws_eip" "mySonarInstance" {
   vpc      = true
   instance = aws_instance.mySonarInstance.id
